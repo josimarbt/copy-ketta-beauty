46eed449270c2ae50ee492fafe628b69d9844102
josimarbt <josimardec@gmail.com>
2023-01-03 16:43:26 -0300

    second commit

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,8 @@
 // Put your application javascript here
+if(window.innerWidth <= 980){
+    let navbar_tablet = document.getElementsByClassName("account-cart-links");
+    let img = "https://icons.veryicon.com/png/o/internet--web/prejudice/user-128.png";
+
+    navbar_tablet.removeChild(navbar_tablet.firstChild);
+    navbar_tablet.appendChild(img);
+}
