9b47c6bf7eb3938897d1fe4136fbe44deaf128b7
josimarbt <josimardec@gmail.com>
2023-01-04 13:52:45 -0300

    responsive navbar

--- assets/application.js
+++ assets/application.js
@@ -1,17 +1,17 @@
 // // Put your application javascript here
 //this code is for hide the links for "login" and "register" on tablet size.
-let tabletSize = window.matchMedia("(max-width: 970px)");
+// let tabletSize = window.matchMedia("(max-width: 970px)");
 
-tabletSize.onchange = (e) => {
-    let menuNavbarLinks = document.getElementById("menu_navbar_links");
-    let navbarIconLinks = document.getElementById("navbar_icon_links");
+// tabletSize.onchange = (e) => {
+//     let menuNavbarLinks = document.getElementById("menu_navbar_links");
+//     let navbarIconLinks = document.getElementById("navbar_icon_links");
 
-    if(e.matches) {
-        menuNavbarLinks.classList.add("hidden");
-        navbarIconLinks.classList.remove("hidden");
-    } else {
-        menuNavbarLinks.classList.remove("hidden");
-        navbarIconLinks.classList.add("hidden");
-    };
-};
+//     if(e.matches) {
+//         menuNavbarLinks.classList.add("hidden");
+//         navbarIconLinks.classList.remove("hidden");
+//     } else {
+//         menuNavbarLinks.classList.remove("hidden");
+//         navbarIconLinks.classList.add("hidden");
+//     };
+// };
 
